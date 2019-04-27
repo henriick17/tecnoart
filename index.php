2be94e0fd9a1eb80a6d0b3242c343aa0fe787ddf
henriick17 <henrickkz.oliveira@gmail.com>
2019-04-27 10:44:07 -0300

    reCommit

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="../css/estilo.css">
+        <link rel="stylesheet" href="./css/estilo.css">
         <title>TECNOART - Sua loja de tecnologia!</title>
     </head>
     <body>
